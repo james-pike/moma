b22a99092a2d211b420ba4dcb984730485a05919
Tester <test@email.com>
2025-01-31 12:53:01 -0500

    commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -18,6 +18,7 @@ const IconBulb = qwikSerialized(() => import("../components/icons/IconBulb"));
 
 import { SITE } from "~/config.mjs";
 import LogoCloud from "~/components/widgets/LogoCloud";
+import Pricing from "~/components/widgets/Pricing";
 
 export default component$(() => {
   return (
@@ -101,6 +102,48 @@ export default component$(() => {
             title: "If you could only ask one question to each person you meet, what would that question be?",
             description:
               "This is not about revenge. This is about justice. A lot of things can change in twelve years, Admiral. Well, that's certainly good to know. About four years. I got tired of hearing how young I looked.",
+          },
+        ]}
+      />
+            <Pricing
+        highlight="Features"
+        title="What you get with Qwinds"
+        subtitle="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae."
+        items={[
+          {
+            title: "Qwik + Tailwind CSS Integration",
+            description:
+              "A seamless integration between two great frameworks that offer high productivity, performance and versatility.",
+            icon: IconBrandTailwind,
+          },
+          {
+            title: "Ready-to-use Component",
+            description:
+              "Widgets made with Tailwind CSS ready to be used in Marketing Websites, SaaS, Blogs, Personal Profiles, Small Business...",
+            icon: IconApps,
+          },
+          {
+            title: "Best Practices",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, quam nec venenatis lobortis, mi risus tempus nulla.",
+          },
+          {
+            title: "Excellent Page Speed",
+            description:
+              "Having a good page speed impacts organic search ranking, improves user experience (UI/UX) and increase conversion rates.",
+            icon: IconRocket,
+          },
+          {
+            title: "Search Engine Optimization (SEO)",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, quam nec venenatis lobortis, mi risus tempus nulla.",
+            icon: IconBrandGoogle,
+          },
+          {
+            title: "Open to new ideas and contributions",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, quam nec venenatis lobortis, mi risus tempus nulla.",
+            icon: IconBulb
           },
         ]}
       />
