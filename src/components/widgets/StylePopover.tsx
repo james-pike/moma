603244621c0fd09cfd3b71dbde923f24c616e2cd
Tester <test@email.com>
2025-01-31 13:40:23 -0500

    commit

--- src/components/widgets/StylePopover.tsx
+++ src/components/widgets/StylePopover.tsx
@@ -8,7 +8,7 @@ import IconBrandTailwind from '../icons/IconBrandTailwind';
 
 export default component$(() => {
   return (
-    <Popover.Root flip={false} gutter={8} preventdefault:scroll>
+    <Popover.Root flip={true} gutter={8} preventdefault:scroll>
       <Popover.Trigger class={buttonVariants({ look: 'outline' })}>
         <IconBrandTailwind/>
       </Popover.Trigger>
