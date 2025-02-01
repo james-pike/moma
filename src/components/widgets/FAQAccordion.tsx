d5cac2e24a0b2bc375718f7331a6a9d33c942dfe
Tester <test@email.com>
2025-02-01 17:48:44 -0500

    commit

--- src/components/widgets/FAQAccordion.tsx
+++ src/components/widgets/FAQAccordion.tsx
@@ -6,20 +6,20 @@ export default component$(() => {
   return (
     <Accordion.Root class="w-full">
       <Accordion.Item>
-        <Accordion.Trigger class="text-md" header="h2">Is it accessible?</Accordion.Trigger>
+        <Accordion.Trigger class="text-lg" header="h2">Is it accessible?</Accordion.Trigger>
         <Accordion.Content>
           Yes. It adheres to the WAI-ARIA sdesign pattern.
         </Accordion.Content>
       </Accordion.Item>
       <Accordion.Item>
-        <Accordion.Trigger  class="text-md" >Is it styled?</Accordion.Trigger>
+        <Accordion.Trigger  class="text-lg" >Is it styled?</Accordion.Trigger>
         <Accordion.Content>
           Yes. It comes with default styles that matches the other components&apos;
           aesthetic.
         </Accordion.Content>
       </Accordion.Item>
       <Accordion.Item>
-        <Accordion.Trigger  class="text-md">Is it animated?</Accordion.Trigger>
+        <Accordion.Trigger  class="text-lg">Is it animated?</Accordion.Trigger>
         <Accordion.Content>
           Yes. It's animated by default, but you can disable it if you prefer.
         </Accordion.Content>
