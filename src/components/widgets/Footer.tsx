3bc8980516aa1861ca93a361fa366bf16dac0e69
Tester <test@email.com>
2025-01-31 21:55:30 -0500

    commit

--- src/components/widgets/Footer.tsx
+++ src/components/widgets/Footer.tsx
@@ -116,7 +116,6 @@ export default component$(() => {
           </ul>
 
           <div class="text-sm text-gray-700 mr-4 dark:text-slate-400">
-            <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 float-left rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
             © 2025 Energy At Work Massage
             · All rights reserved.
           </div>
