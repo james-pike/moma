57e498701524cc178bb92bde14b6e89911728699
Tester <test@email.com>
2025-01-31 21:16:45 -0500

    commit

---
+++ src/components/widgets/ReviewCarousel.tsx
@@ -0,0 +1,20 @@
+import { component$ } from '@builder.io/qwik'
+import { Carousel } from 'flowbite-qwik'
+
+export default component$(() => {
+  return (
+    <Carousel pauseOnHover noControls>
+           <Carousel.Slide>
+        <img src="/images/hero1.jpg" alt="..." />
+      </Carousel.Slide>
+       
+      <Carousel.Slide>
+        <img src="/images/hero2.jpg" alt="..." />
+      </Carousel.Slide>
+      <Carousel.Slide>
+        <img class="object-cover h-full w-full" src="/images/hero3.jpg" alt="..." />
+      </Carousel.Slide>
+
+    </Carousel>
+  )
+})
--- src/routes/(testimonials)/testimonials/index.tsx
+++ src/routes/(testimonials)/testimonials/index.tsx
@@ -1,13 +1,19 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import Stats from "~/components/widgets/Stats";
+import Reviews from "~/components/widgets/Reviews";
 import { SITE } from "~/config.mjs";
 
 
 export default component$(() => {
   return (
     <>
-<Stats/>
+ <Reviews highlight="Features"
+        title="What our clients say"
+        subtitle="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae."
+        items={[
+          
+        ]}
+      />
     </>
   );
 });
