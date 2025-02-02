96120ca2c7757ebe2a62b8336f8d59a6f9676391
Tester <test@email.com>
2025-02-01 21:00:03 -0500

    commit

--- src/components/widgets/Header.tsx
+++ src/components/widgets/Header.tsx
@@ -18,8 +18,8 @@ export default component$(() => {
       id="header"
       class={`sticky top-0 z-40 flex-none mx-auto w-full border-b border-gray-50/0 transition-[opacity] ease-in-out ${
         store.isScrolling
-          ? " md:bg-primary-25/90 md:backdrop-blur-sm dark:md:bg-slate-900/90 bg-primary-25 dark:bg-slate-900"
-          : ""
+          ? " md:bg-primary-25/90 md:backdrop-blur-sm dark:md:bg-slate-900/90 bg-primary-50 dark:bg-slate-900"
+          : "bg-primary-50"
       }`}
       window:onScroll$={() => {
         if (!store.isScrolling && window.scrollY >= 10) {
