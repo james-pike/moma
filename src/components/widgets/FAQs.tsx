2418c51ab8909f08e92759af7f765695fbfcbd67
Tester <test@email.com>
2025-02-02 21:17:15 -0500

    commit

--- src/components/widgets/FAQs.tsx
+++ src/components/widgets/FAQs.tsx
@@ -41,7 +41,7 @@ export default component$((props: Props) => {
         </slot>
       </div>
       <div
-        class={twMerge("relative text-default px-4 md:px-6 py-12 md:py-16 lg:py-20 mx-auto max-w-6xl", isDark ? "dark" : "")}
+        class={twMerge("relative text-default px-4 md:px-6 py-8 md:py-12 lg:py-16 mx-auto max-w-6xl", isDark ? "dark" : "")}
       >
         <Headline
           title={title}
