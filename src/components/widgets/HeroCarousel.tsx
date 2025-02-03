2f1a807181d9d1eebf840bf1ead390b482a61e76
Tester <test@email.com>
2025-02-03 15:32:32 -0500

    commit

--- src/components/widgets/HeroCarousel.tsx
+++ src/components/widgets/HeroCarousel.tsx
@@ -5,16 +5,14 @@ export default component$(() => {
   return (
     <Carousel pauseOnHover noControls>
            <Carousel.Slide>
-        <img src="/images/hero3.webp" alt="..." />
+        <img src="/images/hero1.jpg" alt="..." />
       </Carousel.Slide>
 
       <Carousel.Slide>
         <img class="object-cover h-full w-full" src="/images/hero2.jpg" alt="..." />
       </Carousel.Slide>
        
-      <Carousel.Slide>
-        <img src="/images/hero3.jpg" alt="..." />
-      </Carousel.Slide>
+  
     
 
     
