9103d95e60cee9e2b14d9cf361082ba54c6dac7d
Tester <test@email.com>
2025-02-19 09:38:48 -0500

    commit

--- src/components/widgets/FAQs.tsx
+++ src/components/widgets/FAQs.tsx
@@ -34,7 +34,7 @@ export default component$((props: Props) => {
   } = props;
 
   return (
-    <section class="relativebg-gradient-to-r from-primary-50 to-primary-25" {...(id ? { id } : {})}>
+    <section class="relative bg-gradient-to-r from-primary-50 to-primary-25" {...(id ? { id } : {})}>
       <div class="absolute inset-0 pointer-events-none -z-[1]" aria-hidden="true">
         <slot name="bg">
           <div class={twMerge("absolute inset-0", isDark ? "bg-dark dark:bg-transparent" : "")}></div>
