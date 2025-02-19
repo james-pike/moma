7a16e20f632b6ce7c2b00f573cf74148ffc02453
Tester <test@email.com>
2025-02-18 23:24:24 -0500

    commit

--- src/components/widgets/Hero3.tsx
+++ src/components/widgets/Hero3.tsx
@@ -14,7 +14,7 @@ export default component$(() => {
       
       </Carousel>
       <div class="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center bg-black/50 text-center p-4">
-        <h1 class="text-5xl md:text-7xl font-bold">Welcome to Aroma Cafe</h1>
+        <h1 class="text-5xl md:text-7xl font-bold font-heading">Welcome to Aroma Cafe</h1>
         <p class="text-xl md:text-2xl mt-4">Your Perfect Cup, Every Time</p>
         <div class="mt-6 flex space-x-4">
           <a href="/menu" class="bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-3 px-6 rounded-lg text-lg">View Menu</a>
