ef4735f04b86e6625afa8e2b413876899353a2cf
Tester <test@email.com>
2025-02-19 15:21:27 -0500

    commit

--- src/components/widgets/Hero3.tsx
+++ src/components/widgets/Hero3.tsx
@@ -13,13 +13,13 @@ export default component$(() => {
         </Carousel.Slide>
       </Carousel>
       <div class="absolute inset-0 flex flex-col items-center justify-center bg-black/50 text-center p-4">
-        <h1 class="text-5xl md:text-7xl mb-4 font-bold font-heading">Welcome to Aroma Cafe</h1>
+        <h1 class="text-5xl motion-preset-focus md:text-7xl mb-4 font-bold font-heading">Welcome to Aroma Cafe</h1>
         <div class="max-w-3xl mx-auto lg:max-w-none">
-          <p class="text-2xl text-muted mb-6 dark:text-slate-300">
+          <p class="text-2xl text-muted mb-6 dark:text-slate-300 motion-preset-focus motion-delay-100 ">
 
             Transform your online presence with our custom web design and development services. We help business create stunning, fast, and secure websites.              </p>
             <div class="w-full flex justify-center">
-  <div class="flex flex-col sm:flex-row justify-center gap-2 sm:gap-4 w-full sm:max-w-md lg:max-w-7xl px-4">
+  <div class="flex motion-preset-focus motion-delay-200 flex-col sm:flex-row justify-center gap-2 sm:gap-4 w-full sm:max-w-md lg:max-w-7xl px-4">
     <a href="#menu" class="w-full sm:w-auto">
       <button class="btn w-full max-w-[400px] text-white bg-primary-700 hover:bg-primary-400 dark:bg-primary-700 py-2 rounded-md">
         Explore Menu
