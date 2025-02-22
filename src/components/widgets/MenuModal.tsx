e5c3a948d2800afeaca64b8d51c00707a230834f
Tester <test@email.com>
2025-02-22 16:07:41 -0500

    commit

--- src/components/widgets/MenuModal.tsx
+++ src/components/widgets/MenuModal.tsx
@@ -2,55 +2,78 @@ import { component$, useSignal } from "@builder.io/qwik";
 import { Modal } from "../ui/Modal";
 import { Button, buttonVariants } from "../ui/Button";
 import { LuX } from "@qwikest/icons/lucide";
-import { Label } from "../ui/Label";
-import { Input } from "../ui/Input";
 import { cn } from "@qwik-ui/utils";
 
-
-
 export default component$(() => {
-
-    const show = useSignal(false);
+  const show = useSignal(false);
 
   return (
     <>
-  <Modal.Root bind:show={show}>
-  <Modal.Trigger>Open modal</Modal.Trigger>
-  <Modal.Panel position={"right"}>
-        <Modal.Title>Edit Profile</Modal.Title>
-        <Modal.Description>
-          Make changes to your profile here. Click save when you're done.
-        </Modal.Description>
-        <div class="mt-6">
-          <Label for="name" class="text-right">
-            Name
-          </Label>
-          <Input
-            name="name"
-            id="name"
-            defaultValue="Pedro Duarte"
-            class="col-span-3 w-[300px]"
-          />
-        </div>
-        <footer class="mt-6">
-          <Button look="primary" onClick$={() => (show.value = false)}>
-            Save
-          </Button>
-        </footer>
-        <Modal.Close
-          class={cn(
-            buttonVariants({ size: 'icon', look: 'link' }),
-            'absolute right-3 top-2',
-          )}
-          type="submit"
-        >
-          <LuX class="h-5 w-5" />
-        </Modal.Close>
-      </Modal.Panel>
-</Modal.Root>
+      <Modal.Root bind:show={show}>
+        <Modal.Trigger>Open Navigation</Modal.Trigger>
+        <Modal.Panel position={"left"}> {/* Changed to "left" for sidebar feel */}
+          <Modal.Title>Navigation Menu</Modal.Title>
+          <Modal.Description>
+            Explore your options below.
+          </Modal.Description>
+          
+          {/* Navigation Content */}
+          <nav class="mt-6 space-y-4">
+            <ul class="flex flex-col gap-4">
+              <li>
+                <a
+                  href="#home"
+                  class="block text-gray-700 hover:text-blue-600 font-medium"
+                  onClick$={() => (show.value = false)}
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#profile"
+                  class="block text-gray-700 hover:text-blue-600 font-medium"
+                  onClick$={() => (show.value = false)}
+                >
+                  Profile
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#settings"
+                  class="block text-gray-700 hover:text-blue-600 font-medium"
+                  onClick$={() => (show.value = false)}
+                >
+                  Settings
+                </a>
+              </li>
+              <li>
+                <Button
+                  look="primary"
+                  onClick$={() => {
+                    alert("Logged out!");
+                    show.value = false;
+                  }}
+                  class="w-full text-left"
+                >
+                  Logout
+                </Button>
+              </li>
+            </ul>
+          </nav>
 
+          {/* Close Button */}
+          <Modal.Close
+            class={cn(
+              buttonVariants({ size: "icon", look: "link" }),
+              "absolute right-3 top-2"
+            )}
+            type="submit"
+          >
+            <LuX class="h-5 w-5" />
+          </Modal.Close>
+        </Modal.Panel>
+      </Modal.Root>
     </>
   );
 });
-
-
