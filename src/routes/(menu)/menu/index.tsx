66a1d118cbc4908b0c1043f7b0de1a79ae4d4134
Tester <test@email.com>
2025-02-26 17:07:35 -0500

    commit

---
+++ src/routes/(menu)/menu/index.tsx
@@ -0,0 +1,25 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import Menu from "~/components/widgets/Menu";
+import { SITE } from "~/config.mjs";
+
+export default component$(() => {
+  return (
+    <>
+<Menu title="Gallery Of Past Work"
+              subtitle="Have questions about our café? Find answers to common inquiries below."
+              highlight="Portfolio"
+              items={[]}/>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: SITE.title,
+  meta: [
+    {
+      name: "description",
+      content: SITE.description,
+    },
+  ],
+};
