b71f446b084cb05f5a988ae100b8ce88c8262069
Tester <test@email.com>
2025-02-26 14:28:44 -0500

    commit

--- src/components/widgets/FAQ2.tsx
+++ src/components/widgets/FAQ2.tsx
@@ -1,7 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import { twMerge } from "tailwind-merge";
 import { Headline } from "~/components/ui/Headline";
-import ServicesCarousel from "./ServicesCarousel";
 import FAQAccordion from "./FAQAccordion";
 
 interface Item {
