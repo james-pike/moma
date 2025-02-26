8cd5a2acb98cda90b02c80037ce5661b9957c355
Tester <test@email.com>
2025-02-26 08:40:49 -0500

    commit

--- src/components/widgets/Footer.tsx
+++ src/components/widgets/Footer.tsx
@@ -57,7 +57,7 @@ export default component$(() => {
 
   return (
     <div class="max-w-7xl mx-auto py-2 px-2 bg-gradient-to-r from-primary-300 via-primary-100 to-primary-300">
-      <footer class="border-t border-gray-200 bg-primary-25 dark:border-slate-800 rounded-md py-10 px-4 sm:px-6">
+      <footer class="border-t border-gray-200 bg-primary-25 dark:border-slate-800 rounded-none py-10 px-4 sm:px-6">
         <div class="max-w-7xl mx-auto">
           {/* Top Section */}
           <div class="grid grid-cols-1 md:grid-cols-12 gap-8 mb-12">
