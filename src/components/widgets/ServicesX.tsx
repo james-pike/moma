da4d92739c320972100b7746569639c84c88adc5
Tester <test@email.com>
2025-02-26 15:15:44 -0500

    commit

--- src/components/widgets/ServicesX.tsx
+++ src/components/widgets/ServicesX.tsx
@@ -24,11 +24,11 @@ export default component$((props: Props) => {
   const { id, title = "", subtitle = "", highlight = "",  classes = {}, isDark = false } = props;
 
   return (
-    <section class="relative p-3 bg-primary-50 scroll-mt-16" {...(id ? { id } : {})}>
+    <section class="relative p-2 bg-primary-50 scroll-mt-16" {...(id ? { id } : {})}>
     
       <div
         class={twMerge(
-          "relative mx-auto max-w-5xl bg-white px-3 md:px-6 py-10 md:py-16 lg:py-20 text-default ",
+          "relative mx-auto max-w-5xl bg-white px-4 md:px-6 py-10 md:py-16 lg:py-20 text-default ",
           classes?.container,
           isDark ? "dark" : ""
         )}
