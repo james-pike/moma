da176aa73fc48df5ce384186043b2562b4137251
me@gmail.com <email>
2025-03-03 08:59:28 -0500

    commit

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -5,7 +5,6 @@ import IconTwitter from "../icons/IconTwitter";
 import IconTelegram from "../icons/IconTelegram";
 import { Logo } from "../common/Logo";
 import MenuModal from "../widgets/MenuModal";
-import Analytics from "../icons/Analytics";
 
 export default component$(() => {
   const store = useStore({
@@ -100,8 +99,6 @@ export default component$(() => {
               >
                 Get A Quote
               </a>
-
-              <Analytics />
             <MenuModal />
           </div>
         </div>
