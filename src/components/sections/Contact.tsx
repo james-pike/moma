276c093afd2e42928d5b1d20a45afd0d02851741
me@gmail.com <email>
2025-03-03 20:07:50 -0500

    commit

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -6,7 +6,7 @@ export default component$(() => {
         <>
            <section class="relative p-0 bg-white scroll-mt-16" >
             {/* Form/Contact Section */}
-            <div class="relative isolate mx-auto max-w-6xl bg-gray-100 rounded-lg dark:bg-gray-900">
+            <div class="relative isolate mx-auto max-w-6xl bg-gray-100 dark:bg-gray-900">
                 <div class="grid grid-cols-1 lg:grid-cols-2">
                     <div class="relative px-6 pb-8 pt-10 sm:pt-32 lg:static lg:px-8 lg:py-24">
                         <div class="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
