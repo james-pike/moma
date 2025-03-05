6fbd9558bb8c363bcfc4acfaa0958b1d8f09ce3c
me@gmail.com <email>
2025-03-05 10:09:12 -0500

    Add Tech Stack

--- src/components/widgets/StylePopover.tsx
+++ src/components/widgets/StylePopover.tsx
@@ -16,7 +16,7 @@ export default component$(() => {
         <div class="grid gap-4">
           <div class="space-y-2">
             <h4 class="font-medium leading-none">Dimensions</h4>
-            <p class="text-sm text-muted-foreground">Set the dimensions for the layer.</p>
+            <p class="text-sm text-muted-foreground">Set the dimgensions for the layer.</p>
           </div>
           <div class="grid gap-2">
             <div class="grid grid-cols-5 items-center gap-4">
