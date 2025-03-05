921a2ccf2457dbabd43bf33057e68ffb1944dd7e
me@gmail.com <email>
2025-03-04 20:27:29 -0500

    Add Tech Stack

--- src/components/sections/LogoClouds.tsx
+++ src/components/sections/LogoClouds.tsx
@@ -172,9 +172,9 @@ export default component$((props: Props) => {
                 )}
             >
                 <Headline title={title} subtitle={subtitle} highlight={highlight} classes={classes?.headline} align="center" />
-
+    
                 {/* SVG Grid */}
-                <div class="mt-6 grid grid-cols-3 lg:grid-cols-4 gap-0.1 md:gap-4 xl:gap-8 md:space-y-0 md:mb-8 md:mt-12">
+                <div class="mt-6 grid grid-cols-3 lg:grid-cols-4 gap-0 md:gap-4 xl:gap-8 md:space-y-0 md:mb-8 md:mt-12">
                     {tabContent.map((content, index) => (
                         <div
                             key={index}
@@ -186,25 +186,18 @@ export default component$((props: Props) => {
                                     class={twMerge(
                                         "absolute inset-[-2px] hover:inset-[0px] rounded-none transition-all duration-300",
                                         "border border-gray-200/50",
-                                        selectedIndex.value === index && "bg-gray-400 shadow-[0_0_15px_5px_rgba(59,130,246,0.3)]",
+                                        selectedIndex.value === index && "bg-gray-400 shadow-[0_0_15px_5px_rgba(59,130,246,0.3)] blur-sm",
                                         "group-hover:bg-gray-100 dark:group-hover:bg-gray-900 group-hover:shadow-[0_0_10px_3px_rgba(59,130,246,0.2)]"
                                     )}
                                 />
-                                <div 
-                                    class={twMerge(
-                                        "relative bg-gray-100 hover:bg-gray-200 dark:hover:bg-gray-700 dark:bg-gray-800 w-full p-2",
-                                        "transition-transform duration-300",
-                                        selectedIndex.value === index && "scale-105 blur-sm",
-                                        "group-hover:scale-105"
-                                    )}
-                                >
+                                <div class="relative bg-gray-100 hover:bg-gray-200 dark:hover:bg-gray-700 dark:bg-gray-800 w-full p-2">
                                     {content.icon}
                                 </div>
                             </div>
                         </div>
                     ))}
                 </div>
-
+    
                 {/* Tabs Component */}
                 <Tabs.Root
                     selectedClassName='bg-white'
@@ -216,7 +209,7 @@ export default component$((props: Props) => {
                             <Tabs.Tab key={index} class="px-4 py-2">{content.title}</Tabs.Tab>
                         ))}
                     </Tabs.List>
-
+    
                     {tabContent.map((content, index) => (
                         <Tabs.Panel key={index}>
                             <Card.Root>
