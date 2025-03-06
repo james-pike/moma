2d357251303fc1d85be27ce7d18d01d32a1c9005
me@gmail.com <email>
2025-03-06 08:43:09 -0500

    Add Tech Stack

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -282,7 +282,7 @@ export default component$(() => {
         </div>
       </div>
 
-      <div class="border-b-2 mb-1 border-yellow-200 dark:border-yellow-700 mx-2"></div>
+      <div class="border-b-2 mb-1 border-yellow-200 dark:border-yellow-200 mx-2"></div>
     </header>
   );
 });
