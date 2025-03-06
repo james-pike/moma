fcaf2072022e4b62eda01ee7f44f2e6aaad684b2
me@gmail.com <email>
2025-03-06 13:29:16 -0500

    Add Tech Stack

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -27,7 +27,7 @@ export default component$(() => {
   return (
     <>
 
-<Card0.Root class="mx-1.5 pb-2 ">
+<Card0.Root class="mx-1.5 mb-2 ">
   <Card0.Content>
      <Hero/>
         </Card0.Content>
