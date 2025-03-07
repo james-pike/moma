1a645b88c70ee7bee76937d0f0c0a72ee08dc600
me@gmail.com <email>
2025-03-06 20:34:41 -0500

    Add Tech Stack

--- src/components/sections/Services1.tsx
+++ src/components/sections/Services1.tsx
@@ -1,7 +1,5 @@
 import { component$ } from "@builder.io/qwik";
-import FAQAccordion from "../widgets/FAQAccordion";
-import { ItemGrid2 } from "../ui/ItemGrid2";
-import { PageWrapper } from "../common/PageWrapper";
+
 import { Card } from "../ui/Card";
 import { CardHeadline } from "../ui/CardHeadline";
 import ServicesCarousel from "../widgets/ServicesCarousel";
