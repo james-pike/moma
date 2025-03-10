24686089e5abb1bbbc3c9d2d86f752e6567161c3
me@gmail.com <email>
2025-03-10 16:05:02 -0400

    commit

--- src/root.tsx
+++ src/root.tsx
@@ -40,7 +40,7 @@ export default component$(() => {
                 <link rel="preload" href="/images/hero2.webp" as="image" />
         
       </head>
-      <body class="text-gray-900 max-w-7xl px-3 py-1 mx-auto bg-background dark:text-gray-300 tracking-tight antialiased">
+      <body class="text-gray-900 max-w-7xl px-2 py-1 mx-auto bg-background dark:text-gray-300 tracking-tight antialiased">
        <ObserverProvider>
           <RouterOutlet />
           </ObserverProvider>
