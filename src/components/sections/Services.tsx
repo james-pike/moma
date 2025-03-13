7ffd6458ee290f430fab04ecbd925834df0ddbff
me@gmail.com <email>
2025-03-12 22:50:49 -0400

    commit

--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -1,9 +1,13 @@
 import { component$ } from '@builder.io/qwik';
+
 import { Card } from "../ui/Card";
 import { CardHeadline } from "../ui/CardHeadline";
 import ServicesCarousel from '../widgets/ServicesCarousel';
 
+
+
 interface Props {
+    id?: string;
     title?: any;
     subtitle?: any;
     highlight?: any;
@@ -28,12 +32,15 @@ export default component$((props: Props) => {
                             </svg>
                         </div>
                         <CardHeadline title={title} subtitle={subtitle} highlight={highlight} classes={classes?.headline} align="left" />
+
                     </Card.Header>
                     <Card.Content>
                         <ServicesCarousel />
                     </Card.Content>
                 </div>
+
             </Card.Root>
+
         </section>
     );
 });
